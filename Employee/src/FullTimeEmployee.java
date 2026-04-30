212b5278a829fc87c5d66d2ca32583514f1c999e
rishi-ss7 <rishikasaudhan5152@gmail.com>
2026-04-30 22:52:09 +0530

    F3

---
+++ Employee/src/FullTimeEmployee.java
@@ -0,0 +1,9 @@
+class FullTimeEmployee extends Employee {
+        private double monthlySalary;
+
+        public FullTimeEmployee(String name, int id, double monthlySalary) {
+            super(name, id);
+            this.monthlySalary = monthlySalary;
+        }
+}
+
